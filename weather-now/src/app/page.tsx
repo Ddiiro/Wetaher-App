9b7fa11dd898227f8530eed36e21b1356263cb1b
ddiiro <ddiiro92@gmail.com>
2023-06-21 17:26:17 +0300

    Add the containers for the other two countries - Kenya & Egypt

--- weather-now/src/app/page.tsx
+++ weather-now/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid justify-items-stretch">
+    <div className=" grid justify-items-stretch">
       <nav className="flex p-8 place-content-between h-60 text-white">
 
         <div>
@@ -18,9 +18,8 @@ export default function Home() {
         </div>
     </nav>
 
-    <div className=" grid justify-self-center place-content-center w-96 min-h-[40rem] bg-cyan-300 text-white rounded-lg">
-      <section>
-        <div className="">
+    <div className="flex gap-20 justify-self-center justify-items-stretch">
+        <div className=" grid justtify-self-start place-content-center w-96 min-h-[40rem] bg-cyan-300 text-white rounded-lg">
           <h2 className="text-xl">Kampala</h2>
 
           <h5>Uganda</h5>
@@ -33,7 +32,32 @@ export default function Home() {
             <p>50</p>
           </div>
         </div>
-      </section>
+        <div className=" grid justify-self-center place-content-center w-96 min-h-[40rem] bg-yellow-400 text-white rounded-lg">
+          <h2 className="text-xl">Nairobi</h2>
+
+          <h5>Kenya</h5>
+
+          <h3>Sunny</h3>
+
+          <img src="" alt="" />
+
+          <div>
+            <p>50</p>
+          </div>
+        </div>
+        <div className=" grid justify-self-end place-content-center w-96 min-h-[40rem] bg-rose-600 text-white rounded-lg">
+          <h2 className="text-xl">Cairo</h2>
+
+          <h5>Egypt</h5>
+
+          <h3>Sunny</h3>
+
+          <img src="" alt="" />
+
+          <div>
+            <p>50</p>
+          </div>
+        </div>
     </div>
     </div>
   )
