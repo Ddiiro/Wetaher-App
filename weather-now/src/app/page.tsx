dff830245ef3f944560654fc50c45f0bf12725a2
ddiiro <ddiiro92@gmail.com>
2023-06-21 17:14:35 +0300

    Design the container section of the home page

--- weather-now/src/app/page.tsx
+++ weather-now/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="grid justify-items-stretch">
       <nav className="flex p-8 place-content-between h-60 text-white">
 
         <div>
@@ -17,6 +17,24 @@ export default function Home() {
           </ul>
         </div>
     </nav>
+
+    <div className=" grid justify-self-center place-content-center w-96 min-h-[40rem] bg-cyan-300 text-white rounded-lg">
+      <section>
+        <div className="">
+          <h2 className="text-xl">Kampala</h2>
+
+          <h5>Uganda</h5>
+
+          <h3>Sunny</h3>
+
+          <img src="" alt="" />
+
+          <div>
+            <p>50</p>
+          </div>
+        </div>
+      </section>
+    </div>
     </div>
   )
 }
