0abca941891767e3983ec918b267e7f210175594
Ddiiro <ddiiro92@gmail.com>
2023-06-23 16:27:14 +0300

    Update hover design on the containers

--- weather-now/src/app/page.tsx
+++ weather-now/src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className=" grid justify-items-stretch">
-      <nav className="flex p-8 place-content-between h-24 text-white">
+      <nav className="flex p-8 place-content-between h-32 text-white">
 
         <div>
           <h1 className="text-3xl">
@@ -14,16 +14,16 @@ export default function Home() {
       
         <div className="place-content-between h-48 text-white">
           <ul className=" flex gap-8 text-xl">
-            <li className='group/item hover:bg-rose-600 p-1 rounded-lg'><Link href="/about">Today</Link></li>
-            <li className='group/item hover:bg-rose-600 p-1 rounded-lg'><Link href="/about">This Week</Link></li>
-            <li className='group/item hover:bg-rose-600 p-1 rounded-lg'><Link href="/about">Next Week</Link></li>
+            <li className='hover:border-b-2 p-1 rounded-lg'><Link href="/about">Today</Link></li>
+            <li className='hover:border-b-2 p-1 rounded-lg'><Link href="/about">This Week</Link></li>
+            <li className='hover:border-b-2 p-1 rounded-lg'><Link href="/about">Next Week</Link></li>
           </ul>
         </div>
     </nav>
 
     <div className="flex gap-20 justify-self-center justify-items-stretch">
         <div className=" grid justtify-self-start place-content-center w-96 
-        justify-items-stretch min-h-[20rem] bg-cyan-300 text-white rounded-lg m-0">
+        justify-items-stretch min-h-[20rem] bg-cyan-300 text-white rounded-lg hover:border-4">
           <h2 className="text-2xl">Kampala</h2>
 
           <h5>Uganda</h5>
@@ -43,7 +43,7 @@ export default function Home() {
           </div>
           <button className="rounded-full justify-self-center p-2 bg-black w-1/2 hover:bg-cyan-400"><Link href="/about">See More</Link></button>
         </div>
-        <div className=" grid justify-self-center place-content-center w-96 min-h-[40rem] bg-rose-600 text-white rounded-lg">
+        <div className=" grid justify-self-center place-content-center w-96 min-h-[40rem] bg-rose-600 text-white rounded-lg hover:border-4">
           <h2 className="text-2xl">Nairobi</h2>
 
           <h5>Kenya</h5>
@@ -63,7 +63,7 @@ export default function Home() {
           </div>
           <button className="rounded-full justify-self-center p-2 bg-black w-1/2 hover:bg-rose-400"><Link href="/about">See More</Link></button>
         </div>
-        <div className=" grid justify-self-end place-content-center w-96 min-h-[40rem] bg-yellow-600 text-white rounded-lg">
+        <div className=" grid justify-self-end place-content-center w-96 min-h-[40rem] bg-yellow-600 text-white rounded-lg hover:border-4">
           <h2 className="text-2xl">Cairo</h2>
 
           <h5>Egypt</h5>
